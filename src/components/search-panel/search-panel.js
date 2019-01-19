103fd49a0439f357e47fa16fda90839578821c75
Vasiliy Zozulya <zazevang@gmail.com>
2019-01-19 17:03:31 +0300

    Разбил на компоненты, добавил бутстрап и стилизацию

---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./search-panel.css";
+
+const SearchPanel = () => {
+    return (
+        <input type="text"
+               className="form-control search-panel"
+               placeholder="type to search" />
+    );
+};
+
+export default SearchPanel;
