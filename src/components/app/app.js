9ab6d8871dcbefd6f7236ced374eda1402538147
Vasiliy Zozulya <zazevang@gmail.com>
2019-01-20 13:13:37 +0300

    Добавил функционал добавления новых тасков в лист, подсчет выполненных и невыполненных

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -15,19 +15,23 @@ export default class App extends Component {
 
     state = {
         todoData: [
-            {label: 'Drink Coffee', important: false, id: 1},
-            {label: 'Make Awesome App', important: true, id: 2},
-            {label: 'Have a lunch', important: false, id: 3}
+            this.createTodoItem('Drink Coffee'),
+            this.createTodoItem('Make Awesome App'),
+            this.createTodoItem('Have a lunch')
         ]
     };
 
-    addItem = (text) => {
-        // generate id
-        const newItem = {
-            label: text,
+    createTodoItem(label) {
+        return {
+            label: label,
             important: false,
+            done: false,
             id: this.maxId++
-        };
+        }
+    }
+
+    addItem = (text) => {
+        const newItem = this.createTodoItem(text);
 
         // add element in array
         this.setState(({ todoData }) => {
@@ -58,17 +62,58 @@ export default class App extends Component {
         })
     };
 
+    toggleProperty(arr, id, propName) {
+        const idx = arr.findIndex((el) => el.id === id);
+
+        // 1. update object
+        const oldItem = arr[idx];
+        const newItem = { ...oldItem,
+            [propName]: !oldItem[propName]};
+
+        // 2. construct new array
+        return [
+            ...arr.slice(0, idx),
+            newItem,
+            ...arr.slice(idx + 1)
+        ];
+    };
+
+    onToggleImportant = (id) => {
+        this.setState(({ todoData }) => {
+            return {
+                todoData: this.toggleProperty(todoData, id, 'important')
+            };
+        });
+    };
+
+    onToggleDone = (id) => {
+        this.setState(({ todoData }) => {
+            return {
+                todoData: this.toggleProperty(todoData, id, 'done')
+            };
+        });
+    };
+
+
     render() {
+
+        const { todoData } = this.state;
+        const doneCount = todoData.filter((el) => el.done).length;
+        const todoCount = todoData.length - doneCount;
+
         return (
             <div className="todo-app">
-                <AppHeader toDo={1} done={3}/>
+                <AppHeader toDo={todoCount} done={doneCount}/>
                 <div className="top-panel d-flex">
                     <SearchPanel/>
                     <ItemStatusFilter/>
                 </div>
                 <TodoList
-                    todos={this.state.todoData}
-                    onDeleted={ this.deleteItem }/>
+                    todos={ todoData }
+                    onDeleted={ this.deleteItem }
+                    onToggleImportant={ this.onToggleImportant }
+                    onToggleDone={ this.onToggleDone }
+                />
                 <NewItemPanel
                     onItemAdded={ this.addItem }/>
             </div>
--- src/components/new-item-panel/new-item-panel.js
+++ src/components/new-item-panel/new-item-panel.js
@@ -3,16 +3,35 @@ import React, { Component } from 'react';
 import './new-item-panel.css';
 
 export default class NewItemPanel extends Component {
+
+    state = {
+        label: ''
+    };
+
+    onLabelChange = (e) => {
+      this.setState({
+          label: e.target.value
+      });
+    };
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.onItemAdded(this.state.label);
+    };
+
     render() {
         return (
-            <div className="new-item-panel">
+            <form className="new-item-panel d-flex"
+                    onSubmit={this.onSubmit}>
+                <input type="text"
+                        className="form-control"
+                        onChange={this.onLabelChange}
+                        placeholder="What needs to be done"/>
                 <button
-                    type="button"
-                    className="btn btn-outline-secondary"
-                    onClick={() => this.props.onItemAdded('Hello World')}>
+                    className="btn btn-outline-secondary">
                     New task
                 </button>
-            </div>
+            </form>
         )
     };
 };
